8638f7b16f7d3796a4a167bede47ac3e1326f281
Flag <38012125+Yumiro@users.noreply.github.com>
2020-01-18 22:21:43 +0000

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,7 @@ class Ready {
         this.bot = bot
     }
     async run() {
-        this.bot.editStatus('online', { name: `${this.bot.config.prefix}help`, type: 3 });
+        this.bot.editStatus('online', { name: `over ${this.bot.guilds.size} guilds • ${this.bot.config.prefix}help `, type: 3 });
         console.log('Successfully connected to Discord.');
     }
 }
