1a95db869847dce1c3c0cb0b4f6b8f0293fa7207
Flag <38012125+Yumiro@users.noreply.github.com>
2020-01-19 10:43:58 +0000

    Remove timer from mute.js until further notice
    - Removes timer from the mute command as it wasn't working properly when given an argument that didn't include a number.

--- commands/moderation/mute.js
+++ commands/moderation/mute.js
@@ -6,31 +6,26 @@ class Mute extends Command {
             name: 'mute',
             description: 'Mutes a member in the guild',
             category: '🔨 Moderation',
-            aliases: ['m', 'tempute', 'timeout'],
-            usage: 'mute <user> <time> [reason]'
+            aliases: ['m', 'shut'],
+            usage: 'mute <user> [reason]'
         })
         this.run = async (msg, args) => {
             const user = msg.mentions[0];
-            const time = args[1];
             const reason = `[${msg.author.username}#${msg.author.discriminator}] - ${msg.content.split(' ').slice(2).join(' ') || 'mute command issued'}`;
             let role = msg.channel.guild.roles.find(f => f.name === 'Muted' || f.name === 'muted');
 
             if (user) {
                 if (role) {
-                        if (msg.member.permission.has('manageRoles')) {
-                            if (!msg.channel.guild.members.find(f => f.id === user.id).permission.has('manageRoles')) {
-                                msg.channel.guild.members.find(f => f.id === user.id).addRole(role.id, reason);
-                                msg.channel.createMessage(`${this.bot.emojiList.check} Successfully muted ${user.mention} for ${time}.`);
-
-                                setTimeout(function () {
-                                    msg.channel.guild.members.find(f => f.id === user.id).removeRole(role.id, 'auto unmute');
-                                }, ms(time));
-                            } else {
-                                msg.channel.createMessage(`${this.bot.emojiList.error} This user has the \`Manage Roles\` permission.`);
-                            }
+                    if (msg.member.permission.has('manageRoles')) {
+                        if (!msg.channel.guild.members.find(f => f.id === user.id).permission.has('manageRoles')) {
+                            msg.channel.guild.members.find(f => f.id === user.id).addRole(role.id, reason);
+                            msg.channel.createMessage(`${this.bot.emojiList.check} Successfully muted ${user.mention}.`);
                         } else {
-                            msg.channel.createMessage(`${this.bot.emojiList.error} You don't have the \`Manage Roles\` permission.`);
-                        };
+                            msg.channel.createMessage(`${this.bot.emojiList.error} This user has the \`Manage Roles\` permission.`);
+                        }
+                    } else {
+                        msg.channel.createMessage(`${this.bot.emojiList.error} You don't have the \`Manage Roles\` permission.`);
+                    };
                 } else {
                     try {
                         role = await msg.channel.guild.createRole({
