1abbcca86ff2c8456939217447cb07a43d39df97
Flag <38012125+Yumiro@users.noreply.github.com>
2020-01-19 10:36:45 +0000

    Create dick.js

---
+++ commands/fun/dick.js
@@ -0,0 +1,25 @@
+const Command = require('../../structures/Command');
+class Dick extends Command {
+    constructor(bot) {
+        super(bot, {
+            name: 'dick',
+            description: 'Checks your dick size',
+            category: '😂 Fun',
+            aliases: ['pp', 'cock'],
+            usage: 'dick'
+        })
+        this.run = async (msg) => {
+            const size = msg.author.id.slice(-3) % 20 + 1;
+
+            msg.channel.createMessage({
+                embed: {
+                    color: 0x36393f,
+                    title: size + 'cm',
+                    description: 8 + '='.repeat(size) + 'D'
+                }
+            });
+        };
+    };
+};
+
+module.exports = Dick;
