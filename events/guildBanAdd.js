15aaf8c10ffc20590607f1f891ffca9a8289fd93
Olykir <36753435+LandenStephenss@users.noreply.github.com>
2020-03-09 13:12:13 -0500

    Change the way the guild is grabbed

--- events/guildBanAdd.js
+++ events/guildBanAdd.js
@@ -3,7 +3,8 @@ class guildBanAdd {
         this.bot = bot
     }
     async run(user) {
-        const channel = this.bot.guilds.find(f => f.name === 'atom/dev').channels.find(f => f.id === '613783535630680076');
+        // Just incase someone has another server named 'atom/dev'
+        const channel = this.bot.guilds.find(f => f.name === 'atom/dev' && f.channels.has('613783535630680076')).channels.find(f => f.id === '613783535630680076');
         channel.createMessage({
             embed: {
                 color: 0x36393f,
