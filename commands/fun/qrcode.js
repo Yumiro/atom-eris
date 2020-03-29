13048f39703b3a7f2d25eedc1a5457ab765aaf14
Flag <38012125+Yumiro@users.noreply.github.com>
2020-03-29 15:11:53 +0100

    Create qrcode.js

---
+++ commands/fun/qrcode.js
@@ -0,0 +1,26 @@
+const Command = require('../../structures/Command');
+class QR extends Command {
+    constructor(bot) {
+        super(bot, {
+            name: 'qrcode',
+            description: 'Generates a QR code',
+            category: '😂 Fun',
+            aliases: ['qr'],
+            usage: 'qrcode [text]'
+        })
+        this.run = async (msg, args) => {
+            if (args) {
+                msg.channel.createMessage({
+                    embed: {
+                        color: bot.embedCOLOR,
+                        image: {
+                            url: `https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(args)}`
+                        }
+                    }
+                });
+            }
+        };
+    };
+};
+
+module.exports = QR;
