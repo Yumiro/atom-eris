a581d5cc91d8b8767d0528a4bbd9c8ac1ad2f62f
Olykir <36753435+LandenStephenss@users.noreply.github.com>
2020-05-10 15:41:43 -0500

    this should fix the Database issue
    Make sure that the config location is right in this file and add `dbPass` to it

--- structures/Client.js
+++ structures/Client.js
@@ -1,11 +1,12 @@
 const Eris = require('eris');
+const config = require("../../config.json")
 const { MongoClient } = require("mongodb");
 class client extends Eris {
     constructor(options) {
         super(options)
         this.commands = new Map();
         this.aliases = new Map();
-        MongoClient.connect("mongodb://localhost:27017", (err, client) => { 
+        MongoClient.connect(`mongodb://flag:${config.dbPass}@localhost:27017`, (err, client) => { 
             this.userDB = client.db("atom").collection("users");
             this.guildDB = client.db("atom").collection("guilds");
         })
@@ -13,3 +14,4 @@ class client extends Eris {
 };
 
 module.exports = client;
+// mongodb://Oly:123zserdxcft@157.230.232.81:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false
