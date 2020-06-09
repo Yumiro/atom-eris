d6f842fda85fe27214a6244d8d2cd0e7c4b02a0b
Flag <38012125+Yumiro@users.noreply.github.com>
2020-06-09 17:45:17 +0100

    Add iotd.js (finally)

---
+++ commands/fun/iotd.js
@@ -0,0 +1,32 @@
+const Command = require('../../structures/Command');
+const Parser = require('rss-parser');
+const parser = new Parser();
+class IOTD extends Command {
+    constructor(bot) {
+        super(bot, {
+            name: 'iotd',
+            description: 'Sends an image of the day taken from NASA',
+            category: '😂 Fun',
+            aliases: ['nasa-iotd', 'nasaiotd', 'image-of-the-day', 'imageoftheday', 'nasa-image', 'nasaimage'],
+            usage: 'iotd [user]'
+        })
+        this.run = async (msg) => {
+            const feed = await parser.parseURL('https://www.nasa.gov/rss/dyn/lg_image_of_the_day.rss');
+            const item = feed.items[0];
+
+            msg.channel.createMessage({
+                embed: {
+                    color: bot.embedCOLOR,
+                    title: item.title,
+                    description: item.content,
+                    url: item.link,
+                    image: {
+                        url: item.enclosure.url
+                    },
+                }
+            });
+        };
+    };
+};
+
+module.exports = IOTD;
