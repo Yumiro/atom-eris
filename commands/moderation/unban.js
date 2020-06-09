04008710c140a3c6d06e50f17f088e7c9d2224e2
Flag <38012125+Yumiro@users.noreply.github.com>
2020-06-09 16:40:48 +0100

    Update unban.js

--- commands/moderation/unban.js
+++ commands/moderation/unban.js
@@ -10,13 +10,13 @@ class Unban extends Command {
         })
         this.run = async (msg, args) => {
             const user = msg.channel.guild.getBan(args[0]);
-            const reason = `[${msg.author.username.replace(/[^\x00-\x7F]/g, "")}#${msg.author.discriminator}] - ${msg.content.split(' ').slice(2).join(' ') || 'ban command issued (no reason given)'}`;
+            const reason = `[${msg.author.username.replace(/[^\x00-\x7F]/g, "")}#${msg.author.discriminator}] - ${msg.content.split(' ').slice(2).join(' ') || 'unban command issued (no reason given)'}`;
 
             if (!msg.member.permission.has('banMembers')) {
                 msg.channel.createMessage(`${this.bot.emojiList.error} You don't have the \`Ban Members\` permission.`);
             } else {
                 if (user) {
-                        msg.channel.guild.unbanMember(user, reason);
+                        msg.channel.guild.unbanMember(user.user.id, reason);
                         msg.channel.createMessage(`${this.bot.emojiList.check} Successfully unbanned ${user.mention}.`);
                 } else {
                     msg.channel.createMessage(`${this.bot.emojiList.error} User not found.`);
