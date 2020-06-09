13d6018763a1dd37d276641e1d6d3601d92bd627
Flag <38012125+Yumiro@users.noreply.github.com>
2020-06-09 16:19:09 +0100

    Update unmute.js

--- commands/moderation/unmute.js
+++ commands/moderation/unmute.js
@@ -10,7 +10,7 @@ class Unmute extends Command {
             usage: 'unmute <user> [reason]'
         })
         this.run = async (msg, args) => {
-            const user = msg.channel.guild.members.find(f => f.id === msg.mentions[0]) || msg.channel.guild.members.find(f => f.id === args[0]);
+            const user = msg.channel.guild.members.find(f => f.id === msg.mentions[0].id) || msg.channel.guild.members.find(f => f.id === args[0]);
             const reason = `[${msg.author.username.replace(/[^\x00-\x7F]/g, "")}#${msg.author.discriminator}] - ${msg.content.split(' ').slice(2).join(' ') || 'unmute command issued (no reason given)'}`;
             let role = msg.channel.guild.roles.find(f => f.name === 'Muted' || f.name === 'muted');
 
