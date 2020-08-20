37af40ebae3e75c67ceb0cfbd1eacb9646ff6969
Flag <38012125+Yumiro@users.noreply.github.com>
2020-08-20 21:43:25 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -5,6 +5,8 @@ class Ready {
     async run() {
         this.bot.editStatus('online', { name: `over ${this.bot.guilds.size} guilds • ${this.bot.config.prefix}help `, type: 3 });
         console.log('Successfully connected to Discord.');
+        const wait = require('util').promisify(setTimeout);
+        wait(5000);
         const invites = {};
         this.bot.guilds.forEach(g => {
             g.getInvites().then(guildInvites => {
