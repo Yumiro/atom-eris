5d55701975304eb3a0b5db30576fdebe984a982c
Flag <38012125+Yumiro@users.noreply.github.com>
2020-08-20 21:38:58 +0100

    i'm dumb this is not lua

--- events/ready.js
+++ events/ready.js
@@ -5,7 +5,6 @@ class Ready {
     async run() {
         this.bot.editStatus('online', { name: `over ${this.bot.guilds.size} guilds • ${this.bot.config.prefix}help `, type: 3 });
         console.log('Successfully connected to Discord.');
-        wait(1000);
         const invites = {};
         bot.guilds.forEach(g => {
             g.getInvites().then(guildInvites => {
