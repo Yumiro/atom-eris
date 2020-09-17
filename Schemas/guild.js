e25667337ad8cbb080588cb69820f1a43beba1fe
Sarkis <36753435+LandenStephenss@users.noreply.github.com>
2020-09-17 11:12:39 -0500

    soon:tm: maybe

--- Schemas/guild.js
+++ Schemas/guild.js
@@ -1,5 +1,19 @@
 module.exports = (guild) => {
   return {
-      "_id": guild.id
+     _id: guild.id,
+    prefix: "/",
+    modlogs: new Array(),
+    channels: {
+      modlog: null,
+      welcome: null,
+    },
+    log: { // what to log
+      deletes: false, // log deleted messages
+      joins: false,   // log member joins
+      leaves: false,  // log member leaves
+      kicks: false,   // log when a user gets kicked
+      bans: false     // log when a member is banned
+    },
+    
   }
 }
